ca2eff4f65706736843fe44f21c7a6bdfd02fcbb
akukulanski <akukulanski@gmail.com>
2026-03-21 19:38:59 -0300

    radiosity solver: move gap fill to new function

--- src/thermal_solver/radiosity.py
+++ src/thermal_solver/radiosity.py
@@ -4,12 +4,53 @@ from .constants import STEFAN_BOLTZMANN_W_PER_M2_PER_K4, DEEP_SPACE_TEMP_K
 
 
 def solve_radiosity(
-    surfaces: list,
+    areas_m2: np.ndarray | list,
+    emissivities: np.ndarray | list,
     temperatures_K: np.ndarray | list,
     view_factor_matrix: np.ndarray | list[list],
-    infer_losses_to_deep_space: bool = True,
+    check_no_gaps: bool = False,
 ) -> tuple:
     """
+
+    Imlementation.
+    - A: Area
+    - emm: Emissivity
+    - F[i->j]: View factor from surface i to surface j
+    - T: Temperature
+    - J: Radiosity
+
+    J[i] * A[i] = emi[i] * stef_bol * A[i] * T[i]**4 + (1 - emm[i]) * (A[0] *  F[0->i] * J[0] + A[1] * F[1->i] * J[1] + ...)
+                = emi[i] * stef_bol * A[i] * T[i]**4 + (1 - emm[i]) * (A[i] *  F[i->0] * J[0] + A[i] * F[i->1] * J[1] + ...)
+                = emi[i] * stef_bol * A[i] * T[i]**4 + (1 - emm[i]) *  A[i] * (F[i->0] * J[0] +        F[i->1] * J[1] + ...)
+    J[i]        = emi[i] * stef_bol        * T[i]**4 + (1 - emm[i])         * (F[i->0] * J[0] +        F[i->1] * J[1] + ...)
+
+    M . J = b
+
+    For 3 surfaces it would expand to:
+
+        [1 0 0]   [(1 - emi[0])*F[0->0]     (1 - emi[0])*F[0->1]    (1 - emi[0])*F[0->2]]
+    M = [0 1 0] - [(1 - emi[1])*F[1->0]     (1 - emi[1])*F[1->1]    (1 - emi[1])*F[1->2]]
+        [0 0 1]   [(1 - emi[2])*F[2->0]     (1 - emi[2])*F[2->1]    (1 - emi[2])*F[2->2]]
+
+        [J0]
+    J = [J1]
+        [J2]
+
+        [emm[0] * stef_bol * T[0]**4]
+    b = [emm[1] * stef_bol * T[1]**4]
+        [emm[2] * stef_bol * T[2]**4]
+
+
+    G[i] = F[0->i] * J[i] + F[1->i] * J[i] + F[2->i] * J[i] + ...
+         = sum[j=0 to j=N](F[j->i] * J[i])
+
+    Q[i] =  J[i] * A[i] - sum[j=0 to j=N](A[j] * F[j->i] * J[j])
+         =  J[i] * A[i] - sum[j=0 to j=N](A[i] * F[i->j] * J[j])
+         =  J[i] * A[i] - sum[j=0 to j=N](A[i] * G[j])
+         = (J[i]        - sum[j=0 to j=N](       G[j]))) * A[i]
+
+    Q = A . (J - G)
+
 
     Args:
         surfaces: List of surfaces, each containing area_m2 and emmisivity
@@ -28,9 +69,9 @@ def solve_radiosity(
 
     # Inputs
     # Areas
-    areas_m2 = np.array([s.area_m2 for s in surfaces], dtype=float)
+    areas_m2 = np.array(areas_m2, dtype=float)
     # Emissivities
-    emissivities = np.array([s.emissivity for s in surfaces], dtype=float)
+    emissivities = np.array(emissivities, dtype=float)
     # Temperatures in K
     temperatures_K = np.array(temperatures_K, dtype=float)
     # NxN view-factor matrix
@@ -48,7 +89,7 @@ def solve_radiosity(
     # Check for symmetry
     if not np.allclose(area_eff, area_eff.T):
         raise ValueError(
-            f'Consistency check failed: The areas and view factors don't comply with 'Ai * F_i->j == Aj * F_j->i'.\n'
+            f'Consistency check failed: The areas and view factors don\'t comply with "Ai * F_i->j == Aj * F_j->i".\n'
             f'Areas: {areas_m2}\n'
             f'View Factors F[i, j]:\n',
             f'{view_factor_matrix}\n'
@@ -61,27 +102,7 @@ def solve_radiosity(
     if np.any(row_sums > 1 + 1e-10):
         raise ValueError('View factor rows cannot exceed 1')
 
-    if infer_losses_to_deep_space:
-        # Calculate expansion elements
-        F_space = 1.0 - row_sums
-        F_space = np.clip(F_space, 0.0, 1.0)  # Clip potential rounding errors
-
-        # Create the expanded matrix
-        N = len(areas_m2)
-        F_ext = np.zeros((N + 1, N + 1), dtype=float)
-        F_ext[:N, :N] = view_factor_matrix
-        F_ext[:N, N] = F_space
-        aux = areas_m2 * F_space
-        dummy_area = np.sum(aux)
-        F_ext[N, :N] = aux / dummy_area
-        assert np.isclose(sum(F_ext[N, :N]), 1), f'{sum(F_ext[N, :N])}'
-
-        # Update the original vectors/matrixes with the expanded versions
-        areas_m2 = np.append(areas_m2, dummy_area)  # dummy
-        emissivities = np.append(emissivities, 1.0)
-        temperatures_K = np.append(temperatures_K, DEEP_SPACE_TEMP_K)
-        view_factor_matrix = F_ext
-    elif not np.allclose(row_sums, 1.0):
+    if check_no_gaps and not np.allclose(row_sums, 1.0):
         raise ValueError('View factor rows must add up to 1')
 
     # M . J = b
@@ -101,8 +122,59 @@ def solve_radiosity(
     Q = areas_m2 * (J - G)
 
     # Sanity check
-    if infer_losses_to_deep_space and np.abs(Q.sum()) > 1e-6:
+    if np.allclose(row_sums, 1.0) and np.abs(Q.sum()) > 1e-6:
+        # Something went wrong: No gaps but sum of Q non-zero.
         raise ValueError(
             f'Warning: Energy not conserved (Q={Q}; Q.sum() = {Q.sum()})')
 
     return J, G, Q
+
+
+def fill_gaps(
+    areas_m2: np.ndarray | list,
+    emissivities: np.ndarray | list,
+    temperatures_K: np.ndarray | list,
+    view_factor_matrix: np.ndarray | list[list],
+    external_temp_K: float = DEEP_SPACE_TEMP_K,
+):
+    # Areas
+    # [s.area_m2 for s in surfaces], dtype=float)
+    areas_m2 = np.array(areas_m2, dtype=float)
+    # Emissivities
+    # [s.emissivity for s in surfaces], dtype=float)
+    emissivities = np.array(emissivities, dtype=float)
+    # Temperatures in K
+    temperatures_K = np.array(temperatures_K, dtype=float)
+    # NxN view-factor matrix
+    view_factor_matrix = np.array(view_factor_matrix, dtype=float)
+
+    row_sums = view_factor_matrix.sum(axis=1)
+    if np.any(row_sums > 1 + 1e-10):
+        raise ValueError('View factor rows cannot exceed 1')
+
+    # Calculate expansion elements
+    F_space = 1.0 - row_sums
+    F_space = np.clip(F_space, 0.0, 1.0)  # Clip potential rounding errors
+
+    # Create the expanded matrix
+    N = len(areas_m2)
+    F_ext = np.zeros((N + 1, N + 1), dtype=float)
+    F_ext[:N, :N] = view_factor_matrix
+    F_ext[:N, N] = F_space
+    aux = areas_m2 * F_space
+    dummy_area = np.sum(aux)
+    F_ext[N, :N] = aux / dummy_area
+    assert np.isclose(sum(F_ext[N, :N]), 1), f'{sum(F_ext[N, :N])}'
+
+    # Update the original vectors/matrixes with the expanded versions
+    areas_m2 = np.append(areas_m2, dummy_area)  # dummy
+    emissivities = np.append(emissivities, 1.0)
+    temperatures_K = np.append(temperatures_K, external_temp_K)
+    view_factor_matrix = F_ext
+
+    return (
+        areas_m2,
+        emissivities,
+        temperatures_K,
+        view_factor_matrix,
+    )
--- test/test_radiosity.py
+++ test/test_radiosity.py
@@ -2,18 +2,13 @@ import copy
 import numpy as np
 import pytest
 
-from thermal_solver.radiosity import solve_radiosity
-
-
-class Surface:
-    def __init__(self, area_m2, emissivity):
-        self.area_m2 = area_m2
-        self.emissivity = emissivity
+from thermal_solver.radiosity import solve_radiosity, fill_gaps
 
 
 def test_radiosity():
 
-    surfaces = [Surface(10, 0.8), Surface(15, 0.9), Surface(1, 0.5)]
+    areas_m2 = [10, 15, 1]
+    emissivities = [0.8, 0.9, 0.5]
     temperatures_K = [40, 60, 80]
     # NOTE: The following view factor values fullfill the requirement that Ai * F_i->j == Aj * F_j->i
     view_factor_matrix = [
@@ -22,21 +17,80 @@ def test_radiosity():
         [0.0, 0.0, 0.0],
     ]
 
-    # Expected result
-    J_expected = [2.51764645e-01, 6.78176882e-01, 1.16129419e+00]
-    G_expected = [6.78176882e-01, 1.67844101e-01, 3.01346945e-06]
-    Q_expected = [-4.26412238, 7.65499172, 1.16129117]
+    # NOTE: not closed, there are gaps
+    J_expected = [0.25176462, 0.67817678, 1.16129268,]
+    G_expected = [0.67817678, 0.16784308, 0.,]
+    Q_expected = [-4.26412156, 7.65500547, 1.16129268]
 
     J, G, Q = solve_radiosity(
-        surfaces=surfaces,
+        areas_m2=areas_m2,
+        emissivities=emissivities,
         temperatures_K=temperatures_K,
         view_factor_matrix=view_factor_matrix,
-        infer_losses_to_deep_space=True,
     )
 
-    assert np.allclose(J[:-1], J_expected)
-    assert np.allclose(G[:-1], G_expected)
-    assert np.allclose(Q[:-1], Q_expected)
+    assert np.allclose(J, J_expected)
+    assert np.allclose(G, G_expected)
+    assert np.allclose(Q, Q_expected)
+    # assert np.isclose(np.sum(Q), 0) # Not closed, so unbalanced Q is expected.
+
+    # Filling the gaps with deep space T=0 should give the same result
+    (
+        areas_m2_2,
+        emissivities_2,
+        temperatures_K_2,
+        view_factor_matrix_2,
+    ) = fill_gaps(
+        areas_m2=areas_m2,
+        emissivities=emissivities,
+        temperatures_K=temperatures_K,
+        view_factor_matrix=view_factor_matrix,
+        external_temp_K=0,
+    )
+    J, G, Q = solve_radiosity(
+        areas_m2=areas_m2_2,
+        emissivities=emissivities_2,
+        temperatures_K=temperatures_K_2,
+        view_factor_matrix=view_factor_matrix_2,
+    )
+
+    assert np.allclose(J, [*J_expected, 0.0])
+    assert np.allclose(G, [*G_expected, 0.7586961])
+    assert np.allclose(Q, [*Q_expected, -4.55217658])
+    assert np.isclose(np.sum(Q), 0)
+
+    # Now let's fill the gaps but with deep space temp 2.7K (default)
+    # The result will slightly change because of the non-zero temp.
+    (
+        areas_m2_3,
+        emissivities_3,
+        temperatures_K_3,
+        view_factor_matrix_3,
+    ) = fill_gaps(
+        areas_m2=areas_m2,
+        emissivities=emissivities,
+        temperatures_K=temperatures_K,
+        view_factor_matrix=view_factor_matrix,
+        # external_temp_K=,
+    )
+
+    # Expected result
+    J_expected = [2.51764645e-01, 6.78176882e-01,
+                  1.16129419e+00, 3.01346945e-06]
+    G_expected = [6.78176882e-01, 1.67844101e-01,
+                  3.01346945e-06, 7.58696433e-01]
+    Q_expected = [-4.26412238, 7.65499172, 1.16129117, -4.55216052]
+
+    J, G, Q = solve_radiosity(
+        areas_m2=areas_m2_3,
+        emissivities=emissivities_3,
+        temperatures_K=temperatures_K_3,
+        view_factor_matrix=view_factor_matrix_3,
+    )
+
+    assert np.allclose(J, J_expected)
+    assert np.allclose(G, G_expected)
+    assert np.allclose(Q, Q_expected)
     assert np.isclose(np.sum(Q), 0)
 
     # Check that the solver catches view factor assymmettry
@@ -46,7 +100,8 @@ def test_radiosity():
     bad_view_factor_matrix[1][0] = 1.0 * 11 / 15  # Instead of 1.0 * 10 / 15
     with pytest.raises(ValueError) as excinfo:
         solve_radiosity(
-            surfaces=surfaces,
+            areas_m2=areas_m2,
+            emissivities=emissivities,
             temperatures_K=temperatures_K,
             view_factor_matrix=bad_view_factor_matrix,
         )
@@ -54,13 +109,14 @@ def test_radiosity():
         excinfo.value)
 
     # Check that the solver catches view factors not adding up 1 when
-    # deactivating losses inference.
+    # enabling no gaps check
     with pytest.raises(ValueError) as excinfo:
         solve_radiosity(
-            surfaces=surfaces,
+            areas_m2=areas_m2,
+            emissivities=emissivities,
             temperatures_K=temperatures_K,
             view_factor_matrix=view_factor_matrix,
-            infer_losses_to_deep_space=False,
+            check_no_gaps=True,
         )
     assert str(excinfo.value) == 'View factor rows must add up to 1'
 
@@ -69,7 +125,8 @@ def test_radiosity():
     bad_view_factor_matrix_2[0][0] = 0.1
     with pytest.raises(ValueError) as excinfo:
         solve_radiosity(
-            surfaces=surfaces,
+            areas_m2=areas_m2,
+            emissivities=emissivities,
             temperatures_K=temperatures_K,
             view_factor_matrix=bad_view_factor_matrix_2,
         )
@@ -89,10 +146,11 @@ def test_radiosity():
     Q_expected = [-4.45382136, 4.45382136, 0.0]
 
     J, G, Q = solve_radiosity(
-        surfaces=surfaces,
+        areas_m2=areas_m2,
+        emissivities=emissivities,
         temperatures_K=temperatures_K,
         view_factor_matrix=view_factor_matrix,
-        infer_losses_to_deep_space=False,
+        check_no_gaps=True,
     )
 
     assert np.allclose(J, J_expected)
